8c9afd114006e89dcff4d05e30e59a0fd550ff8a
Ravisankar Ka <ka.ravisankar@gmail.com>
2023-10-04 20:03:47 +0530

    Updated the code with repository pattern

--- src/features/user/user.controller.js
+++ src/features/user/user.controller.js
@@ -1,7 +1,11 @@
 import UserModel from './user.model.js';
+import UserRepository from './user.repository.js';
 import jwt from 'jsonwebtoken';
 
 export default class UserController {
+  constructor(){
+    this.userRepository = new UserRepository();
+  }
    async signUp(req, res) {
     try {
           const {
@@ -10,12 +14,13 @@ export default class UserController {
           password,
           type,
         } = req.body;
-        const user = await UserModel.signUp(
+        const user = new UserModel(
           name,
           email,
           password,
           type
         );
+        await this.userRepository.signUp(user);
         res.status(201).send(user);
     } catch (error) {
       throw new ApplicationError("Something went wrong", 500)
@@ -23,30 +28,35 @@ export default class UserController {
     
   }
 
-  signIn(req, res) {
-    const result = UserModel.signIn(
-      req.body.email,
-      req.body.password
-    );
-    if (!result) {
-      return res
-        .status(400)
-        .send('Incorrect Credentials');
-    } else {
-      // 1. Create token.
-      const token = jwt.sign(
-        {
-          userID: result.id,
-          email: result.email,
-        },
-        'Q2QQLf7zZzdY5BXPSDglSSyjscbdFGeq',
-        {
-          expiresIn: '1h',
+  async signIn(req, res) {
+    try {
+          const result =await this.userRepository.signIn(
+          req.body.email,
+          req.body.password
+        );
+        if (!result) {
+          return res
+            .status(400)
+            .send('Incorrect Credentials');
+        } else {
+          // 1. Create token.
+          const token = jwt.sign(
+            {
+              userID: result.id,
+              email: result.email,
+            },
+            'Q2QQLf7zZzdY5BXPSDglSSyjscbdFGeq',
+            {
+              expiresIn: '1h',
+            }
+          );
+          // 2. Send token.
+          return res.status(200).send(token);
         }
-      );
-
-      // 2. Send token.
-      return res.status(200).send(token);
+    } catch (err) {
+        console.log(err);
+        return res.status(500).send("Something went wrong");
     }
+    
   }
 }
--- src/features/user/user.repository.js
+++ src/features/user/user.repository.js
@@ -1,3 +1,35 @@
+import { ApplicationError } from "../../error-handler/applicationError.js";
+import { getDB } from "../../config/mongodb.js";
+
+
 class UserRepository{
-    
+    async signUp(newUser) {
+        try {
+              // 1. Get the database
+            const db = getDB();
+            // 2. Get the collection
+            const collection = db.collection("users") 
+            // 3. Insert the document
+            await collection.insertOne(newUser);
+            return newUser;
+        } catch (error) {
+              throw new ApplicationError("Something went wrong with the data", 500)
+        }
+      }
+
+      async signIn(email, password) {
+        try {
+              // 1. Get the database
+            const db = getDB();
+            // 2. Get the collection
+            const collection = db.collection("users") 
+            // 3. find the document
+            return await collection.findOne({email, password});
+            
+        } catch (error) {
+              throw new ApplicationError("Something went wrong with the data", 500)
+        }
+      }
 }
+
+export default UserRepository;
