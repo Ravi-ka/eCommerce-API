58951eae68b2234e8e9e868ef02f4155987567ce
Ravisankar Ka <ka.ravisankar@gmail.com>
2023-10-17 22:45:09 +0530

    Mongoose Validation added to the /api/user

--- src/features/user/user.schema.js
+++ src/features/user/user.schema.js
@@ -1,9 +1,18 @@
 import mongoose from "mongoose";
 
 export const userSchema = new mongoose.Schema({
-    name : String,
-    email : {type : String, unique: true},
-    password : String,
+    name : {type: String, maxLength : [25, "Name cannot be more than 25 characters"]},
+
+    email : {type : String, unique: true, required : true, 
+        match : [/.+\@.+\../, "Please enter a valid email address"]},
+
+    password : {type : String, validate:{
+        validator : function(value){
+            return /^(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,12}$/.test(value)
+        },
+        message : "Password should be between 8-12 characters and should contains a special character"
+    } },
+    
     type : {type : String, enum: ['Customer','Seller']}
 
 })
